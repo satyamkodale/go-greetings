145d7750f7e50224dfc0d875590caa6fe75b7238
satyamkodale <satyamkodale@gmail.com>
2025-02-07 16:05:05 +0530

    created greeting func which takes name as agrument and returns the greet message

---
+++ greetings/greetings.go
@@ -0,0 +1,25 @@
+package greetings
+
+import "fmt"
+
+func Greetings(name string) string {
+	// the := operator is a shortcut for declaring and initializing a variable in one line
+	// in Go, every declared variable must be used, or the compiler will throw an error.
+	// var message1 string
+	// message1 = fmt.Sprintf("Hi, %v. Welcome!", name)
+	// println(message1)
+
+	message := fmt.Sprintf("Hi , %v  Welcome!", name)
+	return message
+}
+
+// func main() {
+// 	fmt.Println(Greetings("Satyam"))
+// }
+
+// PS C:\Users\Satyam\OneDrive\Desktop\Goooooooo\go-greetings> go run greetings.go
+// package command-line-arguments is not a main package
+// PS C:\Users\Satyam\OneDrive\Desktop\Goooooooo\go-greetings>
+
+// -> SOLUTION
+// Since greetings.go is part of package greetings, you cannot run it directly using go run greetings.go. Instead, you need a main.go file that imports and calls greetings.Greetings().
